7f0f442c810740c4d5bae4fadda1a1bff93da849
dikie001 <omondidickens255@gmail.com>
2025-06-13 22:00:56 +0300

    Refactor code structure for improved readability and maintainability

--- vite.config.js
+++ vite.config.js
@@ -21,12 +21,12 @@ export default defineConfig({
         theme_color: "#0f172a",
         icons: [
           {
-            src: "/icon-192.png",
+            src: "/test.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "/icon-512.png",
+            src: "/test.png",
             sizes: "512x512",
             type: "image/png",
           },
